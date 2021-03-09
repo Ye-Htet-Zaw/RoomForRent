dd504e7578e226f6c3fc64a1dc1123e7dc5e3841
Ye-Htet-Zaw <yehtetzaw.gic@gmail.com>
2021-03-09 11:24:04 +0630

    YHZ

--- app/src/main/java/com/example/roomforrent/services/RoomForRentService.kt
+++ app/src/main/java/com/example/roomforrent/services/RoomForRentService.kt
@@ -1,4 +1,10 @@
 package com.example.roomforrent.services
 
+import com.example.roomforrent.models.HouseDetails
+import retrofit2.Call
+import retrofit2.http.*
+
 interface RoomForRentService {
+    @GET("houseDetail/{id}")
+    fun getHouseDetailById (@Path("id") id: String) : Call<HouseDetails>
 }
