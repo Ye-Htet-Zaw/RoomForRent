da746952f1991f83192170e852b450d08c0c39c5
nttt111 <nantinttinttun.gic@gmail.com>
2021-04-01 13:59:34 +0630

    //NTTT

--- app/src/main/java/com/example/roomforrent/fragment/LoginProfileFragment.kt
+++ app/src/main/java/com/example/roomforrent/fragment/LoginProfileFragment.kt
@@ -8,17 +8,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.example.roomforrent.R
 import com.example.roomforrent.activity.ChangePasswordActivity
 import com.example.roomforrent.activity.MainActivity
+import com.example.roomforrent.activity.MainActivity.Companion.USERID
 import com.example.roomforrent.activity.PersonalInformationActivity
 import kotlinx.android.synthetic.main.fragment_login_profile.view.*
 
 class LoginProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -32,7 +34,10 @@ class LoginProfileFragment : Fragment() {
             startActivity(Intent(context, PersonalInformationActivity::class.java))
         }
         v.ll_owner_change_password.setOnClickListener {
-            startActivity(Intent(context, ChangePasswordActivity::class.java))
+            val intent=Intent(context,ChangePasswordActivity::class.java)
+            intent.putExtra("UserId",intent.getStringExtra(USERID))
+            startActivity(intent)
+
         }
         //NTTT
         v.btn_owner_profile_logout.setOnClickListener {
