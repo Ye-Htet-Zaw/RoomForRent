4f3f5e127d8b422dfb176c7d2391b4de19e6bcea
nttt111 <nantinttinttun.gic@gmail.com>
2021-04-01 15:32:24 +0630

    //NTTT

--- app/src/main/java/com/example/roomforrent/fragment/LoginProfileFragment.kt
+++ app/src/main/java/com/example/roomforrent/fragment/LoginProfileFragment.kt
@@ -5,11 +5,10 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.roomforrent.R
 import com.example.roomforrent.activity.ChangePasswordActivity
 import com.example.roomforrent.activity.MainActivity
@@ -17,15 +16,13 @@ import com.example.roomforrent.activity.PersonalInformationActivity
 import com.example.roomforrent.models.User
 import com.example.roomforrent.services.ServiceBuilder
 import com.example.roomforrent.services.UserProfileService
+import com.example.roomforrent.utils.Constants.USERID
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_personal_information.*
 import kotlinx.android.synthetic.main.fragment_login_profile.*
 import kotlinx.android.synthetic.main.fragment_login_profile.view.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.text.SimpleDateFormat
-import java.util.*
 
 class LoginProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
@@ -34,18 +31,25 @@ class LoginProfileFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
+        val bundle = arguments
+        val userId = bundle?.getString(USERID)
         val v = inflater.inflate(R.layout.fragment_login_profile, container, false)
+
         v.ll_owner_personal_info.setOnClickListener {
             startActivity(Intent(context, PersonalInformationActivity::class.java))
         }
         v.ll_owner_change_password.setOnClickListener {
-            startActivity(Intent(context, ChangePasswordActivity::class.java))
+            val intent=Intent(context,ChangePasswordActivity::class.java)
+            intent.putExtra(USERID,userId)
+            startActivity(intent)
         }
         //NTTT
         v.btn_owner_profile_logout.setOnClickListener {
