990e1b85c5f56aa4121cb8bde578639d130c7478
nttt111 <nantinttinttun.gic@gmail.com>
2021-05-05 23:56:44 +0630

    //nttt

--- app/src/main/java/com/example/roomforrent/models/UserLogin.kt
+++ app/src/main/java/com/example/roomforrent/models/UserLogin.kt
@@ -6,11 +6,10 @@
 package com.example.roomforrent.models
 
 import java.sql.Timestamp
-import java.util.*
 
-data class UserLogin (
+data class UserLogin(
     val user_id:String,
-    val  user_name:String,
+    val user_name:String,
     val user_email:String,
     val facebook_id:String,
     val password:String,
@@ -18,14 +17,14 @@ data class UserLogin (
     val phone_two:String,
     val user_address:String,
     val user_gender:Int,
-    val user_dob:Date,
+    val user_dob:String,
     val user_position:Int,
     val delete_flag:Int,
-    val delete_datetime:Timestamp,
-    val creator_id:Int,
-    val create_datetime:Timestamp,
-    val updator_id:Int,
-    val update_datetime:Timestamp)
+    val delete_datetime:String,
+    val creator_id:String,
+    val create_datetime: String,
+    val updator_id:String,
+    val update_datetime:String)
 
 
 
