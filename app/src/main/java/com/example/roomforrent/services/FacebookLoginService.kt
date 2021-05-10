3cc337e61a6143370613aa901b5d2efdb4cce300
nttt111 <nantinttinttun.gic@gmail.com>
2021-05-10 10:12:35 +0630

    //Comment

--- app/src/main/java/com/example/roomforrent/services/FacebookLoginService.kt
+++ app/src/main/java/com/example/roomforrent/services/FacebookLoginService.kt
@@ -1,3 +1,11 @@
+/**
+ *
+ * FacebookLoginService
+ *
+ * 2021/04/24 NTTT Create New
+ *
+ * call search api for Facebook
+ */
 package com.example.roomforrent.services
 
 import com.example.roomforrent.models.User
@@ -9,12 +17,15 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface FacebookLoginService {
+    //RegisterFacebookUser
     @POST("createFacebookAccount")
     fun createUser(@Body account: UserLogin): Call<List<UserLogin>>
 
+    //CountFbAccountForDuplicate
     @GET("getFacebookId/{facebook_id}")
     fun getFacebookId(@Path("facebook_id") facebook_id: String):Call<Int>
 
+    //RetrieveUserIdWithFacebookId
     @GET("getUserId/{facebook_id}")
     fun getUserId(@Path("facebook_id") facebook_id: String): Call<UserLogin>
 
