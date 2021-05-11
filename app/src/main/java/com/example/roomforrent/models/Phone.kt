628db90708afbd3d7a97232d4aed6bed2b78872d
Ye-Htet-Zaw <yehtetzaw.gic@gmail.com>
2021-05-11 13:11:54 +0630

    YHZ

--- app/src/main/java/com/example/roomforrent/models/Phone.kt
+++ app/src/main/java/com/example/roomforrent/models/Phone.kt
@@ -3,6 +3,6 @@ package com.example.roomforrent.models
 data class Phone (
     val user_id:String,
     var phone_one: String,
-    var user_position: String,
-    var delete_flag: String
+    var user_position: Int,
+    var delete_flag: Int
 )
